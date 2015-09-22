b48e2174b23aba5a8fd9d6e7e7219acd6909f59e
simonzsolt <simonzsoltjota@gmail.com>
2015-09-22 18:08:56 +0200

    ES index updating

--- routes/search.js
+++ routes/search.js
@@ -34,6 +34,91 @@ Theca.find(function(err, data){
     }
     else{
 
+         for (var i = data.length - 1; i >= 0; i--) {
+
+            var dataId = data[i]._id.toString();
+
+            // for indexing remove 'doc' object from body
+            // client.index({
+
+
+            client.update({
+
+                index: 'theca',
+                type: 'recs',
+                id: dataId,
+                body: {
+                    doc:{
+                        // "id": data[i]._id,
+                        "bh_name": data[i].bh_name,
+                        "origin": data[i].origin,
+                        "doc_date_from": data[i].doc_date_from,
+                        "doc_date_to": data[i].doc_date_to,
+                        "source_type": data[i].source_type,
+                        "ownership": data[i].ownership,
+                        "poss_name": data[i].poss_name,
+                        "poss_name_var": data[i].poss_name_var,
+                        "poss_origin": data[i].poss_origin,
+                        "poss_date": data[i].poss_date,
+                        "poss_place": data[i].poss_place,
+                        "poss_occ": data[i].poss_occ,
+                        "poss_nat": data[i].poss_nat,
+                        "rec_name": data[i].rec_name,
+                        "rec_prev_place": data[i].rec_prev_place,
+                        "rec_curr_place": data[i].rec_curr_place,
+                        "rec_prev_score": data[i].rec_prev_score,
+                        "rec_pub": data[i].rec_pub,
+                        "book_num_from": data[i].book_num_from,
+                        "book_num_to": data[i].book_num_to,
+                        "book_title_num_from": data[i].book_title_num_from,
+                        "book_title_num_to": data[i].book_title_num_to,
+                        "bh_num": data[i].bh_num,
+                        "rec_lang": data[i].rec_lang,
+                        "inherit": data[i].inherit,
+                        "vol_missing_num": data[i].vol_missing_num,
+                        "bibl": data[i].bibl,
+                        "note": data[i].note,
+                        "text": data[i].text,
+                        "prog": data[i].prog,
+                        "created_at": data[i].created_at,
+                        "created_by": data[i].created_by, 
+                        "last_mod": data[i].last_mod,
+                        "mod_by": data[i].mod_by}
+                    }
+
+                }, function (err) {
+
+                    if (err) {
+                        console.log(err);
+                    } else {
+
+                        client.count({
+                            index: 'theca'
+                        }, function(err, res){
+
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log(res);
+                            }
+                        })
+                    }
+                });
+         };
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
         var dump = [];
         for (var i = data.length - 1; i >= 0; i--) {
            
@@ -77,12 +162,11 @@ Theca.find(function(err, data){
 
         };
         // console.log(dump.length);
-/*
-        for (var i = dump.length - 1; i >= 0; i--) {
+
+        for (var i = dump.length - 1; i >= 40; i--) {
             
             var dumpID = dump[i]._id;
             var dumpBody = dump[i];
-            console.log( '>>>>>>>> ' + i);
 
             client.index({
 
@@ -109,20 +193,20 @@ Theca.find(function(err, data){
                     }
                 });
         };
-
-        */
+*/
+        
     }
 })
 
 // =================================QUERIES=========================================
-
+/*
 client.search({
     index:'theca',
     query:{
-        match: {
-            rec_lang: 'latin'
+        term: {
+            bh_name: 'Esztergeom'
         }
     }
 });
-
+*/
 module.exports = router;
