4348f5eba30d8a2cd9053091e16b045924fc52cf
Simon Zsolt <simon.zsolt@mail.com>
2016-11-25 23:05:20 +0100

    merge local

--- routes/search.js
+++ routes/search.js
@@ -73,4 +73,65 @@ router.get('/search/q=:q/gte=:gte/lte=:lte', function (req, res) {
 
 });
 
+router.get('/search/lang/latin', function (req, res) {
+	Theca.find( { 'rec_lang' : new RegExp('latin', 'i') } ).
+		exec(function(err, data){
+			if(err){
+				res.send(err);
+			}
+			else{
+				res.json(data);
+			}
+		})
+});
+
+router.get('/search/lang/german', function (req, res) {
+	Theca.find( { 'rec_lang' : new RegExp('német', 'i') } ).
+		exec(function(err, data){
+			if(err){
+				res.send(err);
+			}
+			else{
+				res.json(data);
+			}
+		})
+});
+
+router.get('/search/lang/italian', function (req, res) {
+	Theca.find( { 'rec_lang' : new RegExp('olasz', 'i') } ).
+		exec(function(err, data){
+			if(err){
+				res.send(err);
+			}
+			else{
+				res.json(data);
+			}
+		})
+});
+
+router.get('/search/origin/:orig', function (req, res) {
+	Theca.find( { 'origin' : new RegExp(req.params.orig, 'i') } ).
+		exec(function(err, data){
+			if(err){
+				res.send(err);
+			}
+			else{
+				res.json(data);
+			}
+		})
+});
+
+router.get('/search/possessor/:poss', function (req, res) {
+
+	Theca.find( { "rec_name": new RegExp(req.params.poss, 'i') } ).
+		exec(function(err, data){
+			if(err){
+				res.send(err);
+			}
+			else{
+				res.json(data);
+			}
+		})
+});
+
 module.exports = router;
